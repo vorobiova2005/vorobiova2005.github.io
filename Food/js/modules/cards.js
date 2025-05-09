4c4a941942f1afe3263a76b03c4350b3f49464c8
Valentina Vorobyova <vorobyova.valentina2005@gmail.com>
2025-05-09 13:28:08 +0300

    Add Food fixed

--- Food/js/modules/cards.js
+++ Food/js/modules/cards.js
@@ -37,7 +37,7 @@ function cards () {
             <div class="menu__item-divider"></div>
             <div class="menu__item-price">
                 <div class="menu__item-cost">Цена:</div>
-                <div class="menu__item-total"><span>${this.price}</span> грн/день</div>
+                <div class="menu__item-total"><span>${this.price}</span> UAH/day</div>
             </div>
         `;
         this.parent.append(element);
