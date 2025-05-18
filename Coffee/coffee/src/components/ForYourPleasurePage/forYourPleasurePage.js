2a47e00d750b9adf906e45b6a21017a5af8b8f98
Valentina Vorobyova <vorobyova.valentina2005@gmail.com>
2025-05-18 15:44:20 +0300

    Add Coffee

---
+++ Coffee/coffee/src/components/ForYourPleasurePage/forYourPleasurePage.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Header from './Header/header';
+/* import Header from './Header/header'; */
+
+
+
+function forYourPleasurePage() {
+  return (
+    <div className="forYourPleasurePage">
+        <Header />
+    </div>
+  );
+}
+
+export default forYourPleasurePage;
